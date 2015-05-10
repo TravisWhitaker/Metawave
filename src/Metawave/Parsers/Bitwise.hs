424409e1c0e8fcf661d169c87c04f557a6c14a18
Travis Whitaker <pi.boy.travis@gmail.com>
2015-05-10 16:14:02 -0400

    Make bit-wise parsers treat their host as Functors.

--- src/Metawave/Parsers/Bitwise.hs
+++ src/Metawave/Parsers/Bitwise.hs
@@ -8,25 +8,25 @@ import Data.Binary.Get
 
 import qualified Data.Attoparsec.ByteString as A
 
-oneoff :: Monad m => Get a -> B.ByteString -> m a
-oneoff = (ifDone .) . pushChunk . runGetIncremental
-    where ifDone (Done _ _ w) = return w
-          ifDone _            = fail "bitwise parse fallthrough"
+oneoff :: Functor f => Get a -> f B.ByteString -> f a
+oneoff = fmap . (checkDone .) . pushChunk . runGetIncremental
+    where checkDone (Done _ _ w) = w
+          checkDone _            = error "Metawave: bitwise parser did not consume all input"
 
 parseWord16be :: A.Parser Word16
-parseWord16be = A.take 2 >>= oneoff getWord16be
+parseWord16be = oneoff getWord16be $ A.take 2
 
 parseWord32be :: A.Parser Word32
-parseWord32be = A.take 4 >>= oneoff getWord32be
+parseWord32be = oneoff getWord32be $ A.take 4
 
 parseWord64be :: A.Parser Word64
-parseWord64be = A.take 8 >>= oneoff getWord64be
+parseWord64be = oneoff getWord64be $ A.take 8
 
 parseWord16le :: A.Parser Word16
-parseWord16le = A.take 2 >>= oneoff getWord16le
+parseWord16le = oneoff getWord16le $ A.take 2
 
 parseWord32le :: A.Parser Word32
-parseWord32le = A.take 4 >>= oneoff getWord32le
+parseWord32le = oneoff getWord32le $ A.take 4
 
 parseWord64le :: A.Parser Word64
-parseWord64le = A.take 8 >>= oneoff getWord64le
+parseWord64le = oneoff getWord64le $ A.take 8
