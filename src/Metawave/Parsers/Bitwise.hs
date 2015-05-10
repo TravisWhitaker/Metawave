62848a8eb832d3ffdaa5aec8125f46d875ca461d
Travis Whitaker <pi.boy.travis@gmail.com>
2015-05-10 13:20:20 -0400

    Finish bit-wise integer parsers.

--- src/Metawave/Parsers/Bitwise.hs
+++ src/Metawave/Parsers/Bitwise.hs
@@ -2,29 +2,31 @@ module Metawave.Parsers.Bitwise where
 
 import Data.Word
 
-import Data.ByteString.Lazy
+import Data.ByteString as B
 
 import Data.Binary.Get
 
-import qualified Data.Attoparsec.ByteString.Lazy as A
+import qualified Data.Attoparsec.ByteString as A
 
-oneoff :: Functor f => Get a -> f ByteString -> f a
-oneoff = fmap . runGet
+oneoff :: Monad m => Get a -> B.ByteString -> m a
+oneoff = (ifDone .) . pushChunk . runGetIncremental
+    where ifDone (Done _ _ w) = return w
+          ifDone _            = fail "bitwise parse fallthrough"
 
 parseWord16be :: A.Parser Word16
-parseWord16be = oneoff getWord16be $ A.take 2
+parseWord16be = A.take 2 >>= oneoff getWord16be
 
---parseWord32be :: A.Parser Word32
---parseWord32be = oneoff getWord32be $ A.take 4
+parseWord32be :: A.Parser Word32
+parseWord32be = A.take 4 >>= oneoff getWord32be
 
---parseWord64be :: A.Parser Word64
---parseWord64be = oneoff getWord64be $ A.take 8
+parseWord64be :: A.Parser Word64
+parseWord64be = A.take 8 >>= oneoff getWord64be
 
---parseWord16le :: A.Parser Word16
---parseWord16le = oneoff getWord16le $ A.take 2
+parseWord16le :: A.Parser Word16
+parseWord16le = A.take 2 >>= oneoff getWord16le
 
---parseWord32le :: A.Parser Word32
---parseWord32le = oneoff getWord32le $ A.take 4
+parseWord32le :: A.Parser Word32
+parseWord32le = A.take 4 >>= oneoff getWord32le
 
---parseWord64le :: A.Parser Word64
---parseWord64le = oneoff getWord64le $ A.take 8
+parseWord64le :: A.Parser Word64
+parseWord64le = A.take 8 >>= oneoff getWord64le
